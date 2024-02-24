aa9e9df096ad5f8ed387404e0799834f65f1cd7a
Mir Raisul Tanvir <tanvirgraphic@gmail.com>
2024-02-24 19:41:26 +0600

    Update

--- src/compoments/Form/ChangePasswordForm.jsx
+++ src/compoments/Form/ChangePasswordForm.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import toast from "react-hot-toast";
 import UserStore from "../../store/UserStore";
 import { useNavigate } from "react-router-dom";
+import { VscEyeClosed } from "react-icons/vsc";
+import { BiShowAlt } from "react-icons/bi";
 
 
 const ChangePasswordForm = () => {
@@ -9,6 +11,10 @@ const ChangePasswordForm = () => {
     const [password, setPassword] = useState("");
     const [confirmPassword, setConfirmPassword] = useState("");
     const navigate = useNavigate()
+
+
+        const [showPassword, setShowPassword] = useState(false);
+    const [showConfirmPassword, setShowConfirmPassword] = useState(false);
 
 
     const handlePassword = (e) =>{
@@ -47,12 +53,22 @@ const ChangePasswordForm = () => {
             <div  className="animate__animated animate__fadeInUp">
                 <form onSubmit={handleSubmit}>
                     <label> New Password</label>
-                    <input onChange={handlePassword } className='form-control p-3' type="password" 
-                        pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"  
-                        title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters"    />
+                    <div className='password-field '>
+                        <input onChange={handlePassword } className='form-control p-3' type={showPassword ? "text" : "password"} 
+                            pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"  
+                            title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters"    />
+                        <span onClick={()=>setShowPassword(!showPassword)} className='password-icon'>  
+                        {showPassword ? <BiShowAlt />   :  <VscEyeClosed />  }
+                        </span>
+                    </div>
 
                     <label className="mt-3">Confirm New Password</label>
-                    <input onChange={handleConfirmPassword } className='form-control p-3 ' type="password"   />
+                    <div className='password-field '>
+                        <input onChange={handleConfirmPassword } className='form-control p-3 ' type={showConfirmPassword ? "text" : "password"}  />
+                        <span onClick={()=>setShowConfirmPassword(!showConfirmPassword)} className='password-icon'>  
+                        {showConfirmPassword ? <BiShowAlt />   :  <VscEyeClosed />  }
+                        </span>
+                    </div>
                 
                     <input className='btn btn-success w-100 p-3 mt-3 animate__animated animate__fadeInUp' type="submit" value="Change Password" />
                 </form>
